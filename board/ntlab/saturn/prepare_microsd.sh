452da902279ede179fac58b4333b520786e1d45e
Andrey Mihadyuk <mihadyuk@ntlab.com>
2015-12-09 10:27:51 +0300

    saturn: Updated preparation scripts for microsd.

--- board/ntlab/saturn/prepare_microsd.sh
+++ board/ntlab/saturn/prepare_microsd.sh
@@ -3,9 +3,9 @@
 #prepare microsd for boot. Before startting create two partiotions on microsd.
 
 #copy linux onto boot partition
-sudo mkfs.ext4 /dev/sdb1
+#sudo mkfs.ext4 /dev/sdb1
 sudo mount /dev/sdb1 /mnt
-#sudo rm -R /mnt/*
+sudo rm -Rf /mnt/*
 sudo tar xf images/rootfs.tar -C /mnt
 sudo cp flash_emmc_linux.sh /mnt/root
 sudo cp flash_emmc_uboot.sh /mnt/root
@@ -13,6 +13,6 @@ sudo umount /mnt
 
 #copy linux onto second partition to be flashed into onboard eMMC.
 sudo mount /dev/sdb2 /mnt
-sudo cp images/rootfs.ext2 /mnt
+sudo cp images/rootfs.tar /mnt
 sudo umount /mnt
 
