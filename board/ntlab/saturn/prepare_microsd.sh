7f885143c42e39b2ff1900114b516f485c4d377e
Andrey Mihadyuk <mihadyuk@ntlab.com>
2015-12-17 10:50:12 +0300

    saturn: Simplified scripts to prepare microsd and flash emmc.

--- board/ntlab/saturn/prepare_microsd.sh
+++ board/ntlab/saturn/prepare_microsd.sh
@@ -6,13 +6,22 @@
 #sudo mkfs.ext4 /dev/sdb1
 sudo mount /dev/sdb1 /mnt
 sudo rm -Rf /mnt/*
+#create root fs
 sudo tar xf images/rootfs.tar -C /mnt
+
+#copy scripts to flash emmc
 sudo cp flash_emmc_linux.sh /mnt/root
 sudo cp flash_emmc_uboot.sh /mnt/root
+
+#copy neccesary files to flash onto emmc
+sudo cp images/u-boot.imx    /mnt/root
+sudo cp images/uboot-env.bin /mnt/root
+sudo cp images/rootfs.tar    /mnt/root
+
 sudo umount /mnt
 
 #copy linux onto second partition to be flashed into onboard eMMC.
-sudo mount /dev/sdb2 /mnt
-sudo cp images/rootfs.tar /mnt
-sudo umount /mnt
+#sudo mount /dev/sdb2 /mnt
+#sudo cp images/rootfs.tar /mnt
+#sudo umount /mnt
 
