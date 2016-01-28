21c94b003760f540be931bf3a83450a10743504e
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-01-28 09:29:33 +0300

    saturn: Added second partition support for eMMC.

--- board/ntlab/saturn/flash_emmc_linux.sh
+++ board/ntlab/saturn/flash_emmc_linux.sh
@@ -15,18 +15,24 @@
 #flashing onboard eMMC
 #mmcblk0p2 - source partition is located on external microsd.
 #mmcblk1p1 - dest eMMC partition.
-mkdir /tmp/dst_part
+dst_part=/tmp/dst_part
+mkdir -p $dst_part
 #mkdir /tmp/src_image
-mount /dev/mmcblk1p1 /tmp/dst_part
+mount /dev/mmcblk1p1 $dst_part
 #mount /dev/mmcblk0p2 /tmp/src_image
-rm -rf /tmp/dst_part/*
+rm -rf $dst_part/*
 
 #tar xf /tmp/src_image/rootfs.tar -C /tmp/dst_part
-tar xf rootfs.tar -C /tmp/dst_part
+tar xf rootfs.tar -C $dst_part
 
-umount /tmp/dst_part
+#create mount point for second partition
+mkdir -p $dst_part/disk-data
+#copy script to automount second partition.
+cp S01mount-second-drive.sh $dst_part/etc/init.d
+
+umount $dst_part
 #umount /tmp/src_image
 
-rm -rf /tmp/dst_part
+rm -rf $dst_part
 #rm -rf /tmp/src_image
 
