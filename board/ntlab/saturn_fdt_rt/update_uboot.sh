855d84363cd52c8529cc1084c633e5026ec98322
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-04-15 14:15:39 +0300

    saturn rt: Added saturn_fdt board to be modified for realtime.

---
+++ board/ntlab/saturn_fdt_rt/update_uboot.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function remove-uboot() {
+         rm -f images/u-boot.imx images/uboot-env.bin
+}
+
+
+wandboard_file=wandboard.c
+wandboard_fullpath=build/uboot-saturn_2016.03/board/wandboard
+wandboard_fullpath_file=$wandboard_fullpath/$wandboard_file
+uboot_patch_dir=/home/user/workspace/buildroot-ntl/board/ntlab/saturn_fdt/patches/u-boot
+
+echo $wandboard_file
+echo $wandboard_fullpath_file
+
+#backup the wandboard.c file
+cp $wandboard_fullpath_file /tmp/
+
+#apply patch to change settings for eMMC and rebuild
+patch $wandboard_fullpath_file $uboot_patch_dir/fix-emmc-init-order.patch
+remove-uboot
+make uboot-rebuild
+
+#save emmc u-boot version
+mv images/u-boot.imx images/u-boot-emmc.imx
+
+#restore original file
+cp /tmp/$wandboard_file $wandboard_fullpath
+rm /tmp/$wandboard_file
+
+
+#make uboot for microsd card
+remove-uboot
+make uboot-rebuild
+
+
+#make
