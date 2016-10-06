6f6377b20e649d547b4357a83bd6455c82b9ffc9
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-10-06 10:36:24 +0300

    saturn fdt rt: Moved stuff from saturn_fdt.

--- board/ntlab/saturn_fdt_rt/copy-to-outdir.sh
+++ board/ntlab/saturn_fdt_rt/copy-to-outdir.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-dst_dir=/home/user/build/output-saturn-fdt
+#dst_dir=/home/user/build/output-saturn
+dst_dir=$1
 
 function copy() {
    echo "copying $1 to $2"
@@ -9,9 +10,17 @@ function copy() {
 copy flash_emmc_linux.sh $dst_dir
 copy flash_emmc_uboot.sh $dst_dir
 copy flash_linux.sh $dst_dir
+copy flash_linux_kernel.sh $dst_dir
 copy flash_uboot.sh $dst_dir
 copy prepare_microsd.sh $dst_dir
 copy update_linux.sh $dst_dir
 copy update_uboot.sh $dst_dir
 copy S01mount-second-drive.sh $dst_dir
+copy run-kexec.sh $dst_dir
+copy S99run-kexec.sh $dst_dir
+copy mount-fs.sh $dst_dir
+copy mount-tracing.sh $dst_dir
+copy trace-syscalls.sh $dst_dir
+copy dump-vmcore.sh $dst_dir
+copy fetch-log.sh $dst_dir
 copy *.patch $dst_dir
