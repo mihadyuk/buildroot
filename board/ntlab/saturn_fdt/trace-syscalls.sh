342047791da1b64c0e6d5a884c354489fec2ab4f
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-10-10 10:41:23 +0300

    saturn fdt: Update trace syscalls script.

--- board/ntlab/saturn_fdt/trace-syscalls.sh
+++ board/ntlab/saturn_fdt/trace-syscalls.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-strace -ttxy -o /root/trace.txt /root/navigator.elf &
+#f tells to trace child process.
+#-e trace=<calls to be traced>
+#in this case we trace open,close,read calls.
+strace -fttxy -e trace=open,close,read,select -o /root/trace.txt /root/navigator.elf &
+#strace -fttxy -o /root/trace.txt /root/navigator.elf &
+
 
