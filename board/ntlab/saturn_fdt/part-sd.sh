a6f2221cd3de56289a399172e9afb4b23907c2af
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-11-01 14:20:26 +0300

    saturn fdt: new scripts to deploy images on sd and emmc.
    - New scripts to deploy on emmc and microsd cards.
    - Force ext4 format after emmc and microsd partitioning.

--- board/ntlab/saturn_fdt/part-sd.sh
+++ board/ntlab/saturn_fdt/part-sd.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+#http://superuser.com/questions/332252/creating-and-formating-a-partition-using-a-bash-script
 SD="$1"
+part_num=1
 (
 echo o #create new empty dos partition
 echo n # add new partition
 echo p #primary partition
-echo 1 #part number
+echo "$part_num" #part number
 echo   # first sector (default 2048)
 echo   # last sector
 echo w # save changes
 ) | sudo fdisk "$SD"
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+./format-ext4.sh "$SD""$part_num"
