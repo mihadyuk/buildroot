033f62702bf82cc6115e02a9e2f05180092dd2ba
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-11-02 16:12:30 +0300

    saturn fdt; New comments.
    Added comment why uboot does not support 64bit mode. The comment just copied from uboot git repo.

--- board/ntlab/saturn_fdt/format-emmc-ext4.sh
+++ board/ntlab/saturn_fdt/format-emmc-ext4.sh
@@ -7,6 +7,25 @@ SD="$1"
 #if the partition created on archlinux mke2fs 1.43.3 (04-Sep-2016).
 #disabling 64bit (-O ^64bit) solves the issue.
 
+#look at git://git.denx.de/u-boot.git 6f94ab6656ceffb3f2a972c8de4c554502b6f2b7
+#ext4: Refuse to mount filesystems with 64bit feature set
+#
+#With e2fsprogs after 1.43 the 64bit and metadata_csum features are
+#enabled by default.  The metadata_csum feature changes how
+#ext4_group_desc->bg_checksum is calculated, which would break write
+#support.  The 64bit feature however introduces changes such that it
+#cannot be read by implementations that do not support it.  Since we do
+#not support this, we must not mount it.
+
+#Cc: Stephen Warren <swarren@nvidia.com>
+#Cc: Simon Glass <sjg@chromium.org>
+#Cc: Lukasz Majewski <l.majewski@samsung.com>
+#Cc: Stefan Roese <sr@denx.de>
+#Reported-by: Andrew Bradford <andrew.bradford@kodakalaris.com>
+#Signed-off-by: Tom Rini <trini@konsulko.com>
+#
+#Hence the uboot does not support 64bit ext4 partitions.
+
 #sudo mkfs.ext4 -v -O ^large_file,^64bit,uninit_bg "$SD"
 mkfs.ext4 -v -O ^64bit "$SD"
 
