2083c365972aae7275f551ff441efeefaaa2629a
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-11-03 10:16:47 +0300

    ntlab: Removed unused saturn board.

--- board/ntlab/saturn/flash_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#sudo dd if=images/rootfs.ext2 of=/dev/sdb1 conv=noerror,sync
-#sudo mkfs.ext4 /dev/sdb1
-sudo mount /dev/sdb1 /mnt
-sudo rm -R /mnt/*
-sudo tar xf images/rootfs.tar -C /mnt
-sudo cp flash_emmc_linux.sh /mnt/root
-sudo cp flash_emmc_uboot.sh /mnt/root
-sudo umount /mnt
--- board/ntlab/saturn/flash_uboot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo dd if=images/u-boot.imx bs=512 seek=2 of=/dev/sdb
-sudo dd if=images/uboot-env.bin bs=512 seek=768 of=/dev/sdb
